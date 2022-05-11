e487c441e59dcde174bc27a068415a1fad1aedd8
KienKHMT <100120769+KienKHMT@users.noreply.github.com>
2022-05-12 00:26:10 +0700

    Module Tim Kiem

---
+++ QL_Kho/Gui/UC_TimKiem.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QL_Kho.Gui
+{
+    partial class UC_TimKiem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_tennv = new System.Windows.Forms.TextBox();
+            this.dgv_tknv = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_tknv)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(387, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Tên NCC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Tên HH";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(493, 176);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 31);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "Nhập tên nhà cc";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_tennv
+            // 
+            this.txt_tennv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tennv.Location = new System.Drawing.Point(131, 176);
+            this.txt_tennv.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txt_tennv.Multiline = true;
+            this.txt_tennv.Name = "txt_tennv";
+            this.txt_tennv.Size = new System.Drawing.Size(207, 31);
+            this.txt_tennv.TabIndex = 14;
+            this.txt_tennv.Text = "Nhập tên hàng hóa";
+            this.txt_tennv.Click += new System.EventHandler(this.txt_tennv_Click);
+            this.txt_tennv.TextChanged += new System.EventHandler(this.txt_tennv_TextChanged);
+            // 
+            // dgv_tknv
+            // 
+            this.dgv_tknv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_tknv.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgv_tknv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_tknv.Location = new System.Drawing.Point(0, 235);
+            this.dgv_tknv.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.dgv_tknv.Name = "dgv_tknv";
+            this.dgv_tknv.RowHeadersWidth = 51;
+            this.dgv_tknv.Size = new System.Drawing.Size(729, 225);
+            this.dgv_tknv.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(729, 129);
+            this.panel1.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(454, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÌM KIẾM HÀNG HÓA";
+            // 
+            // UC_TimKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_tennv);
+            this.Controls.Add(this.dgv_tknv);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "UC_TimKiem";
+            this.Size = new System.Drawing.Size(729, 460);
+            this.Load += new System.EventHandler(this.UC_TimKiem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_tknv)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_tennv;
+        private System.Windows.Forms.DataGridView dgv_tknv;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QL_Kho/Gui/UC_TimKiem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_Kho.Gui
+{
+    public partial class UC_TimKiem : UserControl
+    {
+        DataTable dt;
+        public UC_TimKiem()
+        {
+            InitializeComponent();
+        }
+        private void xuat()
+        { dt= BUS.BUS.xuat_hh_tk();
+            dgv_tknv.DataSource = dt;
+        }
+
+        private void UC_TimKiem_Load(object sender, EventArgs e)
+        {
+            xuat();
+        }
+
+        private void txt_tennv_TextChanged(object sender, EventArgs e)
+        {
+            string st = string.Format("[Tên Hàng Hóa] like '%{0}%'", txt_tennv.Text);
+            dt.DefaultView.RowFilter = st;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string st = string.Format("[Tên NCC] like '%{0}%'", textBox1.Text);
+            dt.DefaultView.RowFilter = st;
+        }
+
+        private void txt_tennv_Click(object sender, EventArgs e)
+        {
+            txt_tennv.Text = "";
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
